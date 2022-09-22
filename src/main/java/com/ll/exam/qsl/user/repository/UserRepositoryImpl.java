fa5b2c4963fbe3ece134007bce22360efd8096f5
JeongHyeSoo <nailonly@naver.com>
2022-09-23 03:51:39 +0900

    검색, Page 리턴, 테스트케이스 추가

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -4,6 +4,8 @@ import com.ll.exam.qsl.user.entity.QSiteUser;
 import com.ll.exam.qsl.user.entity.SiteUser;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -62,4 +64,8 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
     }
 
 
+    @Override
+    public Page<SiteUser> searchQsl(String kw, Pageable pageable) {
+        return null;
+    }
 }
